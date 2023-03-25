ec029eebd77518309a9d70317efd7035956dca5b
Mamaco <126521895+TalkyK@users.noreply.github.com>
2023-03-24 21:31:08 -0300

    fdsefwesf
    sada\df

--- exz021/script.js
+++ exz021/script.js
@@ -4,12 +4,29 @@ function verificar() {
     console.log(numero1)
     var numero2 = Number(document.getElementById('number2').value)
 
+    var numero3 = Number(document.getElementById('number3').value)
+
     if (numero1 > numero2) {
-        document.getElementById("resultado").innerHTML =  `O número ${numero1} é maior`;
-    } else if (numero1 < numero2) {
-        document.getElementById("resultado").innerHTML =  `O número ${numero2} é maior`;
+        if (numero1 > numero2) {
+            document.getElementById("resultado").innerHTML = `O número ${numero1} e maior`
+        } else {
+            document.getElementById("resultado").innerHTML = `O número ${numero2} e maior `
+        }
+    } else if (numero2 > numero3) {
+        if (numero2 > numero3) {
+            document.getElementById("resultado").innerHTML = `O número ${numero2} e maior`
+        } else {
+            document.getElementById("resultado").innerHTML = `O número ${numero3} e maior`
+        }
+    } else if (numero1 == numero2 == numero3) {
+        if (numero1 == numero2 == numero3) {
+            document.getElementById("resultado").innerHTML = `Os números são iguais`
+        } else {
+            document.getElementById("resultado").innerHTML = `O número ${numero3} e maior`
+        }
     } else {
-        document.getElementById("resultado").innerHTML = `<p>O ${numero1} é igual ao ${numero2}</p>`
-    } 
+        document.getElementById("resultado").innerHTML = `O númeor ${numero3} e maior`
+    }
+
 
 }
